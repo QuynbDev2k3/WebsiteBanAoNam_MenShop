c6e796a4b88df292d5bd0580a77099b652b3449a
TheAnh2003 <thean13321@gmail.com>
2024-02-25 16:26:05 +0700

    DBContext

---
+++ APP_DATA/Context/MyDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP_DATA.Context
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+
+        }
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=WINDOWS10\\SQLEXPRESS;Database=TestNew;Trusted_Connection=True;TrustServerCertificate=True");
+            }
+        }
+    }
+
+}

1366fdfca55351317c8e1b1b99329a8d687a0ffe
hoangthihongnghia <109934071+hoangthihongnghia@users.noreply.github.com>
2024-02-28 15:28:51 +0700

    Update IBaseService.cs

--- APP_API/IServices/IBaseService.cs
+++ APP_API/IServices/IBaseService.cs
@@ -1,13 +1,1 @@
-using System;
-using System.Collections.Generic;
-namespace AppData.Serviece.Interfaces
-{
-    public interface IBaseService<T,ID>
-    {
-        T Save(T item);
-        T GetById(ID id);
-        IEnumerable<T> GetAll();
-        void Remove(ID id);
-        PagedList<T> GetPaged(int page, int size);
-    }
-}
+

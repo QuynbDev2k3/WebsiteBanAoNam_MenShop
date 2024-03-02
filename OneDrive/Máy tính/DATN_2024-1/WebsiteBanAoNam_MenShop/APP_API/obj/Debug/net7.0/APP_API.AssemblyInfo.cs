32564a1cf332417be6955c918a84b386768ba5e1
vanloidz75 <vanloindbn@gmail.com>
2024-03-02 17:54:22 +0700

    LoiTV

--- OneDrive/Máy tính/DATN_2024-1/WebsiteBanAoNam_MenShop/APP_API/obj/Debug/net7.0/APP_API.AssemblyInfo.cs
+++ OneDrive/Máy tính/DATN_2024-1/WebsiteBanAoNam_MenShop/APP_API/obj/Debug/net7.0/APP_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APP_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea94200e226d38fea8a6da10a35f9e45e64056e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e9abdb8a22e5afd07f7ef23fc0ad5f98d729034")]
 [assembly: System.Reflection.AssemblyProductAttribute("APP_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APP_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

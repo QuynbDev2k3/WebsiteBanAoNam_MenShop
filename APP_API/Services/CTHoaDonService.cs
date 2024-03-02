ff3b4691c3ea30f76b3817ca47c2fde5dceb8968
vanloidz75 <vanloindbn@gmail.com>
2024-03-02 18:56:50 +0700

    Đẩy chatlieu, voucher, CTHoadon, hoadon

---
+++ APP_API/Services/CTHoaDonService.cs
@@ -0,0 +1,84 @@
+﻿using APP_API.IServices;
+using APP_DATA.Context;
+using APP_DATA.IRepositories;
+using APP_DATA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APP_API.Services
+{
+    public class CTHoaDonService
+    {
+        private MyDbContext _dbcontext;
+        private readonly DbSet<CTHoaDon> _dbset;
+        public CTHoaDonService()
+        {
+            _dbcontext = new MyDbContext();
+            _dbset = _dbcontext.Set<CTHoaDon>();
+
+
+        }
+
+        public void Add(CTHoaDon p)
+        {
+
+            try
+            {
+
+                _dbset.Add(p);
+                _dbcontext.SaveChanges();
+                return;
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        public IEnumerable<CTHoaDon> GetAll()
+
+        {
+            return _dbset.ToList();
+        }
+
+
+        public void Update(CTHoaDon item)
+        {
+            var p = GetAll().FirstOrDefault(p => p.Id == item.Id);
+
+            try
+            {
+                p.dongia = item.dongia;
+                p.soluong = item.soluong;
+                p.trangthai = item.trangthai;
+
+                _dbset.Update(p);
+                _dbcontext.SaveChanges();
+                return;
+            }
+            catch (Exception)
+            {
+
+
+
+            }
+        }
+
+        public void Delete(Guid id)
+        {
+            var p = GetAll().FirstOrDefault(p => p.Id == id);
+            try
+            {
+                _dbset.Remove(p);
+                _dbcontext.SaveChanges();
+                return;
+            }
+            catch (Exception)
+            {
+
+
+
+            }
+        }
+    }
+}
---
+++ APP_DATA/Models/CTHoaDon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP_DATA.Models
+{
+    public class CTHoaDon
+    {
+        public Guid Id { get; set; }
+        //FK
+        public Guid IdHoaDon { get; set; }
+        public Guid IdCTSP { get; set; }
+        public int dongia { get; set; }  
+        public int soluong { get; set; }
+        public int trangthai { get; set; }
+    }
+}

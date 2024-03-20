010d3dcb98ce88c422d42359a2cdc84d3876515d
Vanloidz75 <vanloindbn@gmail.com>
2024-03-20 15:43:43 +0700

    update view Admin

---
+++ APP_VIEW/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using APP_VIEW.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APP_VIEW.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        //private readonly DbContext _context;
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- APP_VIEW/Program.cs
+++ APP_VIEW/Program.cs
@@ -23,5 +23,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "Areas",
+    pattern: "{area:exists}/{controller=Product}/{action=Index}/{id?}");
 
 app.Run();
